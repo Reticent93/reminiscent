dc4070931db5a5cf219a16e4ccf31f7c2c31c772
gren8 <greg.renfro@gmail.com>
2021-06-19 20:56:58 -0700

    added styles for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-
+import {Container, AppBar, Typography, Grid, Grow} from "@material-ui/core";
+import memories from './images/memories.png'
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from './styles.js'
 
 const App = () => {
-  return (
-    <div>
-    <h1>App</h1>
-    </div>
-  );
+    const classes = useStyles()
+    return (
+        <Container maxWidth={"lg"}>
+            <AppBar position={"static"} className={classes.appBar} color={"inherit"}>
+                <Typography className={classes.heading} variant={"h2"} align={"center"}>Reminiscent</Typography>
+                <img className={classes.image} src={memories} alt={'memories'} height={'60'} />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify={"space-between"} alignItems={"stretch"} spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+
+    );
 }
 
 export default App;
