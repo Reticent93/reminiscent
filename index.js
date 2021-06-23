8228b6bfabae69df9560bb7845f0d508159e82a7
gren8 <greg.renfro@gmail.com>
2021-06-23 09:45:23 -0700

    fixed some folder language

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from  'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
@@ -7,22 +7,25 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 dotenv.config()
-app.use(bodyParser.json({limit: "30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
 app.get('/', (req, res) => {
-    res.send('Hellp to Reminisce API')
-}) 
-
-
-
+  res.send('Hellp to Reminisce API')
+})
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
-.then(() =>app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)) )
-.catch((err)=> console.error(err.message))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
+  )
+  .catch((err) => console.error(err.message))
 
 mongoose.set('useFindAndModify', false)
