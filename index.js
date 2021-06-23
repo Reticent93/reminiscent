64cecc0a668325ae06300625ce32a3baa147ea83
gren8 <greg.renfro@gmail.com>
2021-06-23 10:38:44 -0700

    changed files

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 dotenv.config()
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
+
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
