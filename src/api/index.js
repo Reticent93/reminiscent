3914067fbcb44422f9ee4e44be40fe03ca22b400
gren8 <greg.renfro@gmail.com>
2021-06-23 19:42:10 -0700

    added correct api route

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const url = 'https://sharp-easley-038e05.netlify.app/posts'
+const url = 'https://reminisce-project.herokuapp.com/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
