370f8ced0bc906fdaca4f2e00063395c7dc2abee
gren8 <greg.renfro@gmail.com>
2021-06-24 10:39:37 -0700

    added correct api route

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
 app.use(cors)
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use('/posts', postRoutes)
 app.get('/', (req, res) => {
     res.send('Hello to Reminisce API')
