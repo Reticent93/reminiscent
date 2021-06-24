aa560cf2b43164d36576a339634a5f01dffda057
gren8 <greg.renfro@gmail.com>
2021-06-23 19:42:11 -0700

    added correct api route

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 dotenv.config()
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({limit: '30mb', extended: true}))
+app.use(express.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
 app.use('/posts', postRoutes)
 app.get('/', (req, res) => {
-  res.send('Hellp to Reminisce API')
+    res.send('Hellp to Reminisce API')
 })
 
 const PORT = process.env.PORT || 5000
-const MONGO = process.env.CONNECTION_URL
+
 
 mongoose
-  .connect(MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
-  )
-  .catch((err) => console.error(err.message))
+    .connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
+    )
+    .catch((err) => console.error(err.message))
 
 mongoose.set('useFindAndModify', false)
