6cececec2523a1df92a19e90f4bf59352defce9f
gren8 <greg.renfro@gmail.com>
2021-06-24 10:24:26 -0700

    added correct api route

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
@@ -9,11 +8,11 @@ const app = express()
 dotenv.config()
 app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
-app.use(cors())
+app.use(cors)
 
 app.use('/posts', postRoutes)
 app.get('/', (req, res) => {
-    res.send('Hellp to Reminisce API')
+    res.send('Hello to Reminisce API')
 })
 
 const PORT = process.env.PORT || 5000
