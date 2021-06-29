68d7eaa627b6c79bed5fb961b4cf7de5a09f427b
gren8 <greg.renfro@gmail.com>
2021-06-29 13:03:27 -0700

    updated index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express()
 app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
-app.use(cors)
+app.use(cors())
 
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
